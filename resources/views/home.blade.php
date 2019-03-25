e0ece71c428c4ffb6348c94b0997808ba11bc9ad
Paulo Mariano <paulotiagocm@hotmail.com>
2019-03-24 23:10:26 -0300

    add vuejs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Painel</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Bem vindo!
-                </div>
-                        <a href="{{ url('/voluntario') }}">voluntario</a>
-            </div>
-        </div>
-    </div>
-</div>
+<example-component></example-component>
 @endsection
